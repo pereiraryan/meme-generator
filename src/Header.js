3f965816ae9391a7da1b8de6e3d2044ccb9d1662
ryan.p <ryan.p@endurance.com>
2021-04-11 15:47:33 +0530

    Meme Generator

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return(
+
+        <Header>
+             <img 
+                src="https://ryanpereira.me/img/logo.png" 
+                alt="Problem?"
+            />
+            <p>Meme Generator</p>
+        </Header>
+    )
+}
+export default Header
