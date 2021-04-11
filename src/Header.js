703069d269b4da76ec4148bb667a97b9a03e2a6c
ryan.p <ryan.p@endurance.com>
2021-04-11 22:36:55 +0530

    Meme Generator link updated

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header>
             <img 
-                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                src="https://ryanpereira.me/img/logo.png" 
                 alt="Problem?"
             />
             <p>Meme Generator</p>
